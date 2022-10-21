28bafe045692f07ad146ab05d9d5b11eb37777b3
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2022-10-21 14:00:23 +0300

    Added initial player position and some objects for the game.

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,11 @@ const gameOver=document.querySelector('game-over');
 
 gameStart.addEventListener('click',onGameStart);
 
+document.addEventListener('keydown',onKeyDown);
+document.addEventListener('keyup',onKeyUp);
+
+let keys={};
+
 function onGameStart(){
    gameStart.classList.add('hide');
 
@@ -13,4 +18,30 @@ function onGameStart(){
    wizard.style.top='200px';
    wizard.style.left='200px';
    gameArea.appendChild(wizard);
+
+   window.requestAnimationFrame(gameAction);
+}
+
+function onKeyDown(event){
+    keys[event.code]=true;
+}
+
+function onKeyUp(event){
+    keys[event.code]=false;
+}
+
+function gameAction(){
+    if(keys.ArrowUp){
+
+    }
+    if(keys.ArrowDown){
+
+    }
+    if(keys.ArrowLeft){
+
+    }
+    if(keys.ArrowRight){
+        
+    }
+    window.requestAnimationFrame(gameAction);
 }

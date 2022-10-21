c31d73a2143ac47d5c7285ce8eae76691b1e68be
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2022-10-21 12:40:54 +0300

    Added wizard and base style for him.

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,10 @@ gameStart.addEventListener('click',onGameStart);
 
 function onGameStart(){
    gameStart.classList.add('hide');
+
+   const wizard=document.createElement('div');
+   wizard.classList.add('wizard');
+   wizard.style.top='200px';
+   wizard.style.left='200px';
+   gameArea.appendChild(wizard);
 }
